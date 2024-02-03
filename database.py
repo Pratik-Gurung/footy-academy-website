8597f97df89d076fcdecd12fe743ed1e5369c992
Pratik-Gurung <98984613+Pratik-Gurung@users.noreply.github.com>
2024-02-03 18:25:00 +0000

    added connection to database

--- database.py
+++ database.py
@@ -1,6 +1,9 @@
 from sqlalchemy import create_engine, text
+import os
 
-db_connection_string = "mysql+pymysql://1kjvtaq3qs50aw1ogso6:pscale_pw_eLZgn4DstA5BjmFQGGZqrqNJfGnKuWHFZAg6rxXxwzF@aws.connect.psdb.cloud/footyacademy?charset=utf8mb4"
+db_connection_string = os.environ['DB_CONNECTION_STRING']
+
+
 
 engine = create_engine(
   db_connection_string, 
@@ -10,20 +13,10 @@ engine = create_engine(
     }  
   })
 
-#with engine.connect() as conn:
-  #result = conn.execute(text("select * from training_groups"))
-
-  #result_dicts = []
-  #for row in result.all():
-    #result_dicts.append(row._mapping)
-
-  #print(result_dicts)
-  
-  #print("type(result):", type(result))
-  #result_all = result.all()
-  #print("type(result.all()):", type(result_all))
-  #first_result = result_all[0]
-  #print("type(first_result):", type(first_result))
-  #first_result_dict = first_result._mapping ###USE ._mapping TO GET THE DICTIONARY
-  #print("type(first_result_dict):", type(first_result_dict))
-  #print(first_result_dict)
+def load_training_groups_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from training_groups"))
+    training_groups = []
+    for row in result.all():
+      training_groups.append(row._mapping)
+    return training_groups
